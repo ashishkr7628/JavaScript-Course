fafd1ee12e73411ec35ad63e5cad1b536390450e
ashishkr7628 <aashidare123@gmail.com>
2023-11-10 05:47:18 +0530

    first commit

---
+++ datatypes.js
@@ -0,0 +1,7 @@
+let name= "Ashish"
+let age = 18
+let isLoggedIn = false
+
+console.log(typeof "Ashish")
+console.log(typeof null)
+console.log(typeof undefined)
---
+++ variables.js
@@ -0,0 +1,8 @@
+const accountId = 12345
+let accountEmail = "ashishkr123@gmail.com"
+var accountPassword= "12345"
+accountCity = "Cuttack"
+let accountState;
+
+// console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
